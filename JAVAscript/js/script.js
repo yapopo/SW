827500c0f53a230661656623281c477130be6a99
yapopo <dbrkdhk4120@naver.com>
2023-06-08 16:48:43 +0900

    ✨ alert open/close 기능구현

---
+++ JAVAscript/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('open-btn').addEventListener('click', function(){
+  document.getElementById('alert').style.display = 'flex'
+});
+
+document.getElementById('close-btn').addEventListener('click', function(){
+  document.getElementById('alert').style.display = 'none'
+});
