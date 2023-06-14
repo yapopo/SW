ba121b66b0c86d51b591ea378f162d2c6dda5a68
yapopo <dbrkdhk4120@naver.com>
2023-06-14 16:10:46 +0900

    ✨

--- 230613_javascript/javascript/javascript.js
+++ 230613_javascript/javascript/javascript.js
@@ -1,3 +1,18 @@
-document.getElementById("push").onclick = function(){
-  alert("누르지 말랫잖아오");
-}
+// document.getElementById("push").onclick = function(){
+//   alert("누르지 말랫잖아오");
+// }
+
+// var arr = [210,92,"남자",false,null];
+
+// alert("금랑의 키는"+arr[0]+"입니다.");
+// alert("금랑의 몸무게는"+arr[1]+"입니다.");
+// alert("금랑의 성별은"+arr[2]+"입니다.");
+
+var house = [
+  ["가","나","다"],
+  ["라","마","바"],
+  ["사","아","자"],
+  ["차","카","타"],
+  ["파"],
+
+];
