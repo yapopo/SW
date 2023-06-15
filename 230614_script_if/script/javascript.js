ed802add9c950f04f480c471a97d515ce50e73ae
yapopo <dbrkdhk4120@naver.com>
2023-06-15 14:36:32 +0900

    ✨ if이중문 /for

--- 230614_script_if/script/javascript.js
+++ 230614_script_if/script/javascript.js
@@ -37,15 +37,115 @@
 
 // and 조건 연습
 
-var english = 50;
+var english = 58;
 var credit = 120;
 var certificate = true;
 var foriner = true;
+var korean = 52;
 
-if(english >= 60 && credit >=120 && certificate){
-  alert("졸업 축하합니다!");
-}else if(foriner && english >=0 && credit >=120 && certificate){
-  alert("졸업 축하합니다!");
+// if(english >= 60 && credit >=120 && certificate){
+//   alert("졸업 축하합니다!");
+// }else if(foriner && credit >=120 && certificate){
+//   alert("졸업 축하합니다!");
+// }else{
+//   alert("졸업 요건이 충족되지 않았습니다");
+// }
+
+
+if(foriner){
+  if((korean >= 60 || english >=60) && credit >=120 && certificate){
+    alert("졸업 축하합니다!");
+  }else{
+    alert("졸업 요건이 충족되지 않았습니다");
+  }
 }else{
-  alert("졸업 요건이 충족되지 않았습니다");
-}
+  if(english >= 60 && credit >=120 && certificate){
+    alert("졸업 축하합니다!");
+  }else{
+    alert("졸업 요건이 충족되지 않았습니다");
+  }
+};
+
+
+
+
+// 직장 내 반경 10km(필수)
+// 주차(필수)
+// 엘리베이터(필수)
+
+// 20평 이상이거나 주변상가 1키로 이내(옵션)
+
+// if(직장 && 주차 && 엘리베이터 && (면적||상가))
+
+
+var house = [
+  {
+    work:12,
+    park:true,
+    el:false,
+    area:21,
+    shop:0.9
+  },
+  {
+    work:9,
+    park:true,
+    el:true,
+    area:24,
+    shop:1.2
+  },
+  {
+    work:2,
+    park:true,
+    el:false,
+    area:22,
+    shop:0.2
+  },
+  {
+    work:8,
+    park:true,
+    el:true,
+    area:22,
+    shop:0.5
+  },
+  {
+    work:12,
+    park:true,
+    el:true,
+    area:23,
+    shop:0.9
+  },
+
+]
+
+// var work = 9.8;
+// var park = true;
+// var el = true;
+// var area = 21;
+// var shop = 1.1;
+
+if(house[3].company <= 10 && 
+  house[3].park && house[3].el &&
+  (house[3].area >= 20 || house[3].shop <= 1)){
+  alert("계약하겠습니다!!!");
+}else{
+  alert("다른 집 볼게요...");
+};
+
+console.log(house)
+
+
+
+var sum = 0;
+for(i=1; i<=100; i++){
+  sum = sum + i
+}alert(sum);
+
+
+// if(조건절){}
+//  && : 앤드 조건
+// ((||)) : or 조건
+
+var plus = 0;
+for(i=5; i<=10; i++){
+  plus = plus +i
+}alert(plus);
