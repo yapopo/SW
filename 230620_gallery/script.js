fe2b8b2dd3fb6403cd4a37a7e582949cfc73950a
yapopo <dbrkdhk4120@naver.com>
2023-06-20 16:36:47 +0900

    ✨ contain을 이용한 class 제거

--- 230620_gallery/script.js
+++ 230620_gallery/script.js
@@ -4,7 +4,11 @@ const items = images.querySelectorAll(".img-item");
 
 items.forEach((item) => {
   item.addEventListener("click", () => {
+    if(item.classList.contains("active")){
+      item.classList.remove("active")
+    }else{
     items.forEach((item) => item.classList.remove("active"))
     item.classList.add("active");
+    }
   })
 })
