1e554ab5de94d0e32b6f86bdb5baa8a3c12d5f20
yapopo <dbrkdhk4120@naver.com>
2023-06-26 16:47:11 +0900

    ✨ forEach

--- 230626_list아코디언/script.js
+++ 230626_list아코디언/script.js
@@ -6,6 +6,8 @@ const button = list.querySelectorAll("list__btn")
 
 listItem.forEach((item)=> {
   item.addEventListener("click", ()=> {
+    listItem.forEach(item => item.classList.remove("active"))
     item.classList.toggle("active")
+    console.log(item)
   })
 })
