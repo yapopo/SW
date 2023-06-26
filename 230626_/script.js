03ff326718fa627db50f148c89972872185a6ced
yapopo <dbrkdhk4120@naver.com>
2023-06-26 14:26:45 +0900

    ✨ +1 -1 제거

--- 230626_/script.js
+++ 230626_/script.js
@@ -3,14 +3,14 @@ const title = document.getElementById("title")
 const plus = document.getElementById("plus")
 const minus = document.getElementById("minus")
 
-let index = 16;
+let index = 32;
 
 plus.addEventListener("click" , () => {
   index++
-  title.style.fontSize = `${index + 1}px`
+  title.style.fontSize = `${index}px`
 })
 
 minus.addEventListener("click" , () => {
   index--
-  title.style.fontSize = `${index - 1}px`
+  title.style.fontSize = `${index}px`
 })
