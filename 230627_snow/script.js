5e8874eafed718987aa977d839faa7e11638aea8
yapopo <dbrkdhk4120@naver.com>
2023-06-27 15:10:10 +0900

    ✨ Math.random

--- 230627_snow/script.js
+++ 230627_snow/script.js
@@ -4,9 +4,28 @@ const wrapper = document.getElementById("wrapper")
 let count = 0;
 
 const addSnow = () => {
+
+  let size = Math.random() * (7-3) + 3 + "px"
+  let blur = Math.random() * (3-1) + 1 + "px"
+
   const span = document.createElement("span")
   span.classList.add("snow")
+
+  span.style.left = Math.random() * (window.innerWidth - 1) + 1 + "px"
+  span.style.width = size
+  span.style.height = size
+  span.style.opacity = Math.random();
+  span.style.filter = `blur(${blur})`
+  span.style.animationDelay = Math.random() * (10-1) + 1 + "s"
+  span.style.animationDuration = Math.random() * (12-5) + 5 + "s"
+
+
   wrapper.appendChild(span)
+
+  if(count < 100){
+    count++
+    window.requestAnimationFrame(addSnow)
+  }
 }
 
-addSnow()
+window.requestAnimationFrame(addSnow)
