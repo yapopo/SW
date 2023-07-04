98296374696753c68f0d94583125992655517b8e
yapopo <dbrkdhk4120@naver.com>
2023-07-04 13:37:17 +0900

    ⚡️

--- 230609_LEGO(반응형)/javascript/sdbar.js
+++ 230609_LEGO(반응형)/javascript/sdbar.js
@@ -4,16 +4,33 @@ const sdbarOverlay = document.getElementById('overlay');
 
 console.log(barIcon,sidebar,sdbarOverlay);
 
-barIcon.addEventListener('click', function() {
+// barIcon.addEventListener('click', function() {
 
-  sidebar.classList.add('is-active')
-  sdbarOverlay.classList.add('is-active');
+//   sidebar.classList.add('is-active')
+//   sdbarOverlay.classList.add('is-active');
 
-})
+// })
 
-sdbarOverlay.addEventListener('click',function(){
+// sdbarOverlay.addEventListener('click',function(){
 
-  sidebar.classList.remove('is-active')
-  sdbarOverlay.classList.remove('is-active');
+//   sidebar.classList.remove('is-active')
+//   sdbarOverlay.classList.remove('is-active');
 
-})
+// })
+
+// function openSidebar() {
+//   // 이 클래스가 있을 때 화면이 어떻게 보일건지 css로 먼저 작성
+//   // 자바스크립트로 클래스를 toggle(없으면 추가, 있으면 제거)
+//   // 클래스 탈부착 형식
+//   sidebar.classList.add('is-active');
+//   sdbarOverlay.classList.add('is-active');
+// }
+
+// barIcon.addEventListener('click', openSidebar)
+
+// function closeSidebar() {
+//   sidebar.classList.remove('is-active')
+//   sdbarOverlay.classList.remove('is-active')
+// }
+
+// sdbarOverlay.addEventListener('click', closeSidebar)
