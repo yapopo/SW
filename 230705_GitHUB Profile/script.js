82d483ffc3041a8cb1f74013e427a5c6cc53b150
yapopo <dbrkdhk4120@naver.com>
2023-07-06 15:35:19 +0900

    ⚡️ dropdown 조건문

--- 230705_GitHUB Profile/script.js
+++ 230705_GitHUB Profile/script.js
@@ -2,6 +2,21 @@ const profile = document.querySelector(".profile-img")
 
 const drawerMenu = document.querySelector(".profile")
 
-profile.addEventListener("click", ()=>{
-  drawerMenu.classList.toggle("active")
-})
+// profile.addEventListener("click", (e)=>{
+//   e.preventDefault();
+//   drawerMenu.classList.toggle("active")
+// })
+
+
+profile.addEventListener("click",(e)=>{
+  e.preventDefault();
+    if(this.active){
+      drawerMenu.classList.remove("active")
+    }else{
+      drawerMenu.classList.add("active")
+    }
+   
+    this.active = !this.active
+
+
+  })
