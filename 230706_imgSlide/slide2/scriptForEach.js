30e65d461b43e61c5fa9fad4dc93e5716d79dfcf
yapopo <dbrkdhk4120@naver.com>
2023-07-07 16:49:30 +0900

    ⚡️ button

--- 230706_imgSlide/slide2/scriptForEach.js
+++ 230706_imgSlide/slide2/scriptForEach.js
@@ -4,12 +4,10 @@ buttons.forEach((btn,index)=>{
   btn.addEventListener("click",()=>{
     document.querySelector(".slide-list").style.transform =
     `translateX(${index * -100}vw)`
-    
-    if(this){
-      btn.style.opacity = "0.3"
-    }else{
-      btn.style.opacity = "1"
-    }
+
+    btn.classList.remove("active")
+    btn.classList.add("active")
+
 
   })
 })
