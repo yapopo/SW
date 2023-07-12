a99b8e34a1400be4a5d43657893382cb1786bab9
yapopo <dbrkdhk4120@naver.com>
2023-07-12 14:41:13 +0900

    ✨ simg-slide 기능구현

--- 230711_HSFF 실습/js/script.js
+++ 230711_HSFF 실습/js/script.js
@@ -10,3 +10,13 @@ sideMenuBtn.addEventListener("click", ()=>{
   sideMenu.classList.remove("is-active")
 })
 
+
+let buttons = document.querySelectorAll(".slide-btn-group button")
+
+
+buttons.forEach((btn,index)=>{
+  btn.addEventListener("click", ()=>{
+    let slide = document.querySelector(".slide-list").style.transform =
+    `translateX(${index*-100}vw)`
+  })
+})
