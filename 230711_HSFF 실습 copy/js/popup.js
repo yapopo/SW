20853cca3fb29086d35d573ef89a033b78a91713
yapopo <dbrkdhk4120@naver.com>
2023-07-17 16:48:12 +0900

    ⚡️ modal close

---
+++ 230711_HSFF 실습 copy/js/popup.js
@@ -0,0 +1,6 @@
+let closeBtn = document.querySelector(".modal-close-btn")
+let modal = document.querySelector(".modal")
+
+closeBtn.addEventListener(".click",()=>{
+  modal.style.dispaly = "none"
+})
