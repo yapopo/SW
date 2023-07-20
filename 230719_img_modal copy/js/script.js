7377a6aa050f2df01aff3f927798537e687d0e90
yapopo <dbrkdhk4120@naver.com>
2023-07-20 13:17:33 +0900

    🎉 new copy

---
+++ 230719_img_modal copy/js/script.js
@@ -0,0 +1,12 @@
+
+$(".modal-item").click(function(){
+  let i =$(this).index();
+  $(".modal").eq(i).addClass("active")
+  $(".opacity").addClass("active")
+  console.log(i)
+})
+
+$(".modal-btn button").click(function(){
+  $(".modal").removeClass("active")
+  $(".opacity").removeClass("active")
+})
