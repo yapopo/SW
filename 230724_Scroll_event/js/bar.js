6f0759ce396d29bfa82088e6a08d6028ef8ff0a7
yapopo <dbrkdhk4120@naver.com>
2023-07-24 16:13:49 +0900

    ⚡️ 공식 수정

--- 230724_Scroll_event/js/bar.js
+++ 230724_Scroll_event/js/bar.js
@@ -4,12 +4,14 @@ window.addEventListener("scroll",()=>{
   let 스크롤높이 = document.querySelector("html").scrollHeight;
 
 
-  if(스크롤양 <= 높이){
-    document.querySelector(".bar").style.width = 
-  `${스크롤양/스크롤높이 * 100}%`
-  }
-  else{document.querySelector(".bar").style.width = 
-  `${(스크롤양+높이)/스크롤높이 * 100}%`}
+  // if(스크롤양 <= 높이){
+  //   document.querySelector(".bar").style.width = 
+  // `${스크롤양/스크롤높이 * 100}%`
+  // }
+  // else{document.querySelector(".bar").style.width = 
+  // `${(스크롤양+높이)/스크롤높이 * 100}%`}
 
-  console.log(스크롤양,높이,스크롤높이)
+  document.querySelector(".bar").style.width = 
+  `${스크롤양/(스크롤높이 - 높이) * 100}%`
+
 })
