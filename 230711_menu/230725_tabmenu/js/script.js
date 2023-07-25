82d03f679003d2cef600822f684aecad10a7d82a
yapopo <dbrkdhk4120@naver.com>
2023-07-25 14:42:07 +0900

    ⚡️ for 반복문

--- 230711_menu/230725_tabmenu/js/script.js
+++ 230711_menu/230725_tabmenu/js/script.js
@@ -1,10 +1,20 @@
 
 
-$(".tab-item").click(function(){
-  let i = $(this).index();
+// $(".tab-item").click(function(){
+//   let i = $(this).index();
 
-  $(this).addClass("is-active");
-  $(".tab-item").not(this).removeClass("is-active")
-  $(".tab-content").eq(i).addClass("is-active")
-  $(".tab-content").not($(".tab-content").eq(i)).removeClass("is-active")
-})
+//   $(this).addClass("is-active");
+//   $(".tab-item").not(this).removeClass("is-active")
+//   $(".tab-content").eq(i).addClass("is-active")
+//   $(".tab-content").not($(".tab-content").eq(i)).removeClass("is-active")
+// })
+
+for(let i = 0; i < $(".tab-item").length; i++){
+  $(".tab-item").eq(i).on("click",function(){
+    $(".tab-item").removeClass("is-active")
+    $(".tab-content").removeClass("is-active")
+
+    $(this).addClass("is-active")
+    $(".tab-content").eq(i).addClass("is-active")
+  })
+}
