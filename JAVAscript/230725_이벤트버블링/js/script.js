da31fafe2540d7ceb53a42fbe6cb7544a8212e19
yapopo <dbrkdhk4120@naver.com>
2023-07-26 13:37:08 +0900

    ⚡️ .is()

--- JAVAscript/230725_이벤트버블링/js/script.js
+++ JAVAscript/230725_이벤트버블링/js/script.js
@@ -27,4 +27,14 @@ $(".overlay").on("click",function(e){
   if($(e.target).is(".overlay")){
     $(".overlay").removeClass("show");
   }
+
+  console.log($(e.target))
+  console.log(e.target)
+
+  // jQurey의 e.target은 객체로 반환한다
+  // jQurey에서는 객체비교를 위해서는 ==가 아닌 .is()사용
+  // .is()메서드의 반대는 != 로 사용한다
+  // JS의 == 부등호는 객체의 내용이 아닌 참조를 비교하므로 값이 같아도 False를 반환
+  // 객체가 가지고 있는 실제 값(데이터) / 객체가 저장되어 있는 메모리 주소(참조)
 })
+
