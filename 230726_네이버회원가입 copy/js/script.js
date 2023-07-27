d96daaac98facaa2024077586ee91f0616b5ceef
yapopo <dbrkdhk4120@naver.com>
2023-07-27 16:54:03 +0900

    ⚡️ .checked class 추가 및 check되기

--- 230726_네이버회원가입 copy/js/script.js
+++ 230726_네이버회원가입 copy/js/script.js
@@ -1,42 +1,12 @@
-$("#mainCheck").click(function(){
-  if($("#mainCheck").is(":checked")){
-    $("input:checkbox").prop("checked",true)
+$("label").on("click" , function(e){
+  e.preventDefault();
+
+  $(this).children(".checkbox-img").toggleClass("checked");
+
+  if($(this).children(".checkbox-img").hasClass("checked")){
+    $(this).children('input[type = "checkbox"]').attr("checked",true)
   }else{
-    $("input:checkbox").prop("checked",false)
+    $(this).children('input[type = "checkbox"]').removeAttr("checked")
   }
 
 })
-
-
-// $(".ok").click(function(){
- 
-//   if($("#mustCheck1").is(":checked") == false){
-//     $(".alart").addClass("active")
-//   }else if($("#mustCheck2").is(":checked") == false){
-//     $(".alart").addClass("active")
-//   }
-
-// })
-
-$(".ok").click(function(){
- 
-  if($("#mustCheck1").is(":checked") == false ||
-  $("#mustCheck2").is(":checked") == false){
-    $(".alart").addClass("active")
-  }else if($("#mustCheck1").is(":checked") == true &&
-  $("#mustCheck2").is(":checked") == true){
-    $(".alart").removeClass("active")
-  }
-
-})
-
-
-// $(function(){
-//   if($(".check:checked").length == 4){
-//     $("#mainCheck").prop("checked",false);
-//   }else{
-//     $("mainCheck").prop("checked",true)
-//   }
-
-//   console.log($(".check:checked"))
-// })
