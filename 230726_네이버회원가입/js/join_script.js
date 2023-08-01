9bd71e99aec59a8852059eae5495432527ad89d6
yapopo <dbrkdhk4120@naver.com>
2023-08-01 16:50:21 +0900

    ⚡️ pw script

--- 230726_네이버회원가입/js/join_script.js
+++ 230726_네이버회원가입/js/join_script.js
@@ -1,5 +1,5 @@
 $(".id-txt input").focusout(function(){
-  let content = $(".id-txt input").val().length;
+  let content = $(this).val().length;
 
   if(content == 0){
     $(".id .alert").css("display","block")
@@ -21,4 +21,36 @@ $(".id-txt input").focusout(function(){
 
 $(".pw-input input").focusout(function(){
   let content = $(this).val().length;
+
+  if(content == 0){
+    $(".pw .alert").css("display","block")
+    $(".pw p.alert").text("필수 정보입니다.")
+    $(".pw p.alert").css("color","red")
+    $(".pw-img").removeClass("not-ok")
+    $(".pw-img").removeClass("ok")
+  }else if(content < 8 ||  16 < content){
+    $(".pw .alert").css("display","block")
+    $(".pw p.alert").text("8 ~ 16자의 영문 대 소문자,숫자,특수문자를 사용하세요.")
+    $(".pw p.alert").css("color","red")
+    $(".pw-img").addClass("not-ok")
+  }else{
+    $(".pw .alert").css("display","none")
+    $(".pw-img").removeClass("not-ok")
+    $(".pw-img").addClass("ok")
+  }
+
+})
+
+$(".pw-check input").focusout(function(){
+  let content = $(this).val().length;
+
+  if(content == 0){
+    $(".pw-check .alert").css("display","block")
+    $(".pw-check p.alert").text("필수 정보입니다.")
+    $(".pw-check p.alert").css("color","red")
+    $(".pw-check-img").removeClass("not-ok")
+    $(".pw-check-img").removeClass("ok")
+  }else if()
+
+
 })
