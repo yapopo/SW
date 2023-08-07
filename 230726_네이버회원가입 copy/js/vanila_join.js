fc191a9042ddeb3ab236a17fe4e3a11f3346aefd
yapopo <dbrkdhk4120@naver.com>
2023-08-07 16:47:38 +0900

    ✨ 휴대전화 조건문 구현

--- 230726_네이버회원가입 copy/js/vanila_join.js
+++ 230726_네이버회원가입 copy/js/vanila_join.js
@@ -92,3 +92,125 @@ document.querySelector(".username input").addEventListener("focusout",function()
   }
 
 })
+
+let bitrhList = document.querySelectorAll(".birth-item")
+
+function birthWarnTxt(text){
+  document.querySelector(".birth .warn").innerHTML = 
+  `<span class = 'text-red'>${text}</span>`
+}
+
+bitrhList.forEach(function(item){
+  item.addEventListener("focusout",()=>{
+    let year = bitrhList[0].value;
+    let month = bitrhList[1].value;
+    let date = bitrhList[2].value;
+
+    let now = new Date();
+    let nowStamp = now.getTime();
+    now = now.getFullYear();
+
+    let birth = new Date(year,month,date)
+    birth = birth.getTime();
+
+    if(year.length != 4) {
+      birthWarnTxt("태어난 년도 4자리를 정확하게 입력하세요.")
+    } else if(month.length == 0)  {
+      birthWarnTxt("태어난 월을 입력하세요")
+    }else if(date.length == 0 || date > 31 || date <=0)  {
+      birthWarnTxt("태어난 일(날짜)2자리를 정확하게 입력하세요.")
+    }
+    // isNAN : 매개변수가 숫자인지 검사하는 함수
+    else if(isNaN(year*month*date)){
+      birthWarnTxt('생년월일을 확인해주세요.')
+    } else if(now - year > 100){
+      birthWarnTxt('정말이세요?')
+    } else if(nowStamp < birth){
+      birthWarnTxt('미래에서 오셨군요.^^')
+    } else {
+      bitrhveri = true;
+      birthWarnTxt("")
+    }
+
+  })
+
+})
+
+let genderInputs = document.querySelectorAll(".gender .inputbox")
+
+genderInputs.forEach((input)=>{
+
+  input.addEventListener("click",function(){
+    genderInputs.forEach((input)=>{
+      input.classList.remove("btn-primary")
+    })
+    this.classList.add("btn-primary")
+    this.querySelector("input[type='radio']").checked = true;
+    genderveri = true;
+  })
+})
+
+document.querySelector(".usermail input").addEventListener("focusout",function(){
+  let userMail = this.value;
+  let mailExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  let mailWarn = document.querySelector(".usermail .warn");
+
+  if(userMail.length == 0){
+    mailWarn.innerHTML  = '';
+  }else if(!mailExp.test(userMail)){
+    mailveri = false;
+    mailWarn.innerHTML = "<span class = 'text-red'>이메일을 다시 확인해주세요.</span>"
+  }else{
+    mailveri = true;
+    mailWarn.innerHTML = "";
+  }
+
+  console.log(userMail)
+})
+
+document.querySelector(".phonenum input").addEventListener("focusout",function(){
+  let phoneNum = this.value;
+  let numWarn = document.querySelector(".phone .warn")
+
+
+  if(phoneNum.length == 0){
+    numWarn.innerHTML = essenInfo;
+  }else{
+    numWarn.innerHTML ="";
+  }
+})
+
+document.getElementById("veribtn").addEventListener("click",function(){
+  let phoneNum = document.querySelector(".phonenum input").value;
+  phoneNum = phoneNum.replace(/[^0-9.]/g, '');
+  document.querySelector(".phonenum input").value = phoneNum;
+  let phoneWarn = document.querySelector(".phone .warn")
+  let veriText = document.getElementById("veritext")
+
+  let phoneLen;
+
+  if(phoneNum.length < 10 || phoneNum.length > 11){
+    phoneLen = false;
+  }else{
+    phoneLen = true;
+  }
+
+  let phoneNaN;
+
+  if(!isNaN(phoneNum)){
+    phoneNaN = true;
+  }else{
+    phoneNaN = false;
+  }
+
+  if(phoneLen&&phoneNaN == true){
+    phoneWarn.innerHTML = "<span class='text-green'>인증번호를 발송했습니다.</span>"
+    veriText.disabled = false;
+    veriText.parentNode.classList.remove("disinput")
+  }else{
+    phoneWarn.innerHTML = "<span class ='text-red'>형식에 맞지 않는 번호입니다.</span>"
+    veriText.disabled = true;
+    veriText.parentNode.classList.add("disinput")
+  }
+
+})
