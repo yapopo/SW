ad7f3a2fde7bdbc08512fe1510833fd42a58586b
yapopo <dbrkdhk4120@naver.com>
2023-08-23 16:14:46 +0900

    ⚡️ keyup으로 변경

--- 230823_login-dark/script.js
+++ 230823_login-dark/script.js
@@ -1,36 +1,24 @@
 
-$("#id").keypress(function(){
+$("#id").keyup(function(){
   let userId = $(this).val();
 
   if(userId.length > 0){
     $(".id").addClass("on")
     $("#id").addClass("on")
+  }else{
+    $(".id").removeClass("on")
+    $("#id").removeClass("on")
   }
 })
 
-$("#pw").keypress(function(){
+$("#pw").keyup(function(){
   let userPw = $(this).val();
 
   if(userPw.length > 0){
     $(".pw").addClass("on")
     $("input").addClass("on")
     $(".pw-show").addClass("active")
-  }
-})
-
-$("#id").keydown(function(){
-  let userId = $(this).val();
-
-  if(userId.length == 0){
-    $(".id").removeClass("on")
-    $("#id").removeClass("on")
-  }
-})
-
-$("#pw").keydown(function(){
-  let userPw = $(this).val();
-
-  if(userPw.length == 0){
+  } else {
     $(".pw").removeClass("on")
     $("#pw").removeClass("on")
     $(".pw-show").removeClass("active")
