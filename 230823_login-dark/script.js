50c892826aa54a1473e863515dc70869d426428e
yapopo <dbrkdhk4120@naver.com>
2023-08-23 16:11:51 +0900

    ⚡️ 비밀번호 표시/숨기기

--- 230823_login-dark/script.js
+++ 230823_login-dark/script.js
@@ -10,7 +10,7 @@ $("#id").keypress(function(){
 
 $("#pw").keypress(function(){
   let userPw = $(this).val();
-  
+
   if(userPw.length > 0){
     $(".pw").addClass("on")
     $("input").addClass("on")
@@ -34,5 +34,17 @@ $("#pw").keydown(function(){
     $(".pw").removeClass("on")
     $("#pw").removeClass("on")
     $(".pw-show").removeClass("active")
+  }
+})
+
+$(".pw-show").click(function(){
+  $(this).toggleClass("hide")
+
+  if($(this).hasClass("hide")){
+    $(this).text("비밀번호 숨기기")
+    $("#pw").attr("type","text")
+  }else{
+    $(this).text("비밀번호 표시")
+    $("#pw").attr("type","password")
   }
 })
