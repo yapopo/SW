1a7b68e4bca56ec9ede30affd8dd85a9fa0e3fa2
yapopo <dbrkdhk4120@naver.com>
2023-08-24 16:47:00 +0900

    ✨

--- 230823_login-dark - 복사본/script.js
+++ 230823_login-dark - 복사본/script.js
@@ -1,76 +1,67 @@
+// 화살표 함수 (arrow function)
+// 함수 선언식 보다 간결하게 사용할 수 있다.
+// function name(x, y){
+//   return x + y;
+// }
+// x, y => x + y;
+let btnLog = document.querySelector('.btn-login');
+let darkToggle = document.getElementById('darkmode-toggle');
 
-$("#id").keyup(function(){
-  let userId = $(this).val();
+document.querySelectorAll('.animate-input').forEach((e) => {
+  // e : .animate-input class가진 값을 하나씩 떼서 넘겨준 값
+  let input = e.querySelector('input');
+  let button = e.querySelector('button');
 
-  if(userId.length > 0){
-    $(".id").addClass("on")
-    $("#id").addClass("on")
-    $(".login").attr("disabled", false)
-  }else{
-    $(".id").removeClass("on")
-    $("#id").removeClass("on")
+  // onkeyup
+  // 사용자가 키보드 키를 눌렀다가 떼었을 때 발생하는 이벤트
+  // trim() : 공백을 제거하는 함수
+
+  // 공백을 제거한 input.value.length가 0보다 클 때 (= 입력된 값이 있다)
+  // .animate-input에 active class를 추가해서 span이 위로 올라가게 한다.
+  // .btn-login에 disabled 라는 속성을 remove
+
+  // else
+  // .animate-input 에 active class remove span 원래 자리
+  // btnLog disabled 속성 add => setAttribute
+  input.onkeyup = () => {
+    if(input.value.trim().length > 0) {
+      e.classList.add('active');
+      btnLog.removeAttribute('disabled');
+    } else {
+      e.classList.remove('active');
+      btnLog.setAttribute('disabled', 'true');
+    }
   }
+
+  // 비밀번호 표시 / 숨기기
+  // 비밀번호 표시 클릭 했을 때 => 숨기기 input type="text"
+  // 비밀번호 숨기기 클릭 했을 때 => 표시 input type="password"
+  // getAttribute input type 속성을 가져온다.
+  if(button) {
+    button.onclick = () => {
+      if(input.getAttribute('type') === 'text') {
+        input.setAttribute('type', 'password');
+        button.innerHTML = '비밀번호 표시';
+      } else {
+        input.setAttribute('type', 'text');
+        button.innerHTML = '비밀번호 숨기기';
+      }
+    }
+  } 
 })
 
-$("#pw").keyup(function(){
-  let userPw = $(this).val();
 
-  if(userPw.length > 0){
-    $(".pw").addClass("on")
-    $("input").addClass("on")
-    $(".pw-show").addClass("active")
-  } else {
-    $(".pw").removeClass("on")
-    $("#pw").removeClass("on")
-    $(".pw-show").removeClass("active")
-  }
-})
+// darkmode toggle
+// darkToggle 클릭 했을 때 
+// 텍스트 변경 : Darkmode -> Lightmode
+// body에 .dark class add
 
-$(".pw-show").click(function(){
-  $(this).toggleClass("hide")
+darkToggle.onclick = (e) => {
+  e.preventDefault();
+  let body = document.querySelector('body');
+  body.classList.toggle('dark');
 
-  if($(this).hasClass("hide")){
-    $(this).text("비밀번호 숨기기")
-    $("#pw").attr("type","text")
-  }else{
-    $(this).text("비밀번호 표시")
-    $("#pw").attr("type","password")
-  }
-})
-
-$("input").keyup(function(){
-  let idLen = $("#id").val().length;
-  let pwLen = $("#pw").val().length;
-  let len = idLen + pwLen
-
-  console.log(len)
-
-  if(len == 0){
-    $(".login").attr("disabled", true)
-  }else{
-    $(".login").attr("disabled", false)
-  }
-})
-
-$(".mode").click(function(){
-  $(".wrapper").toggleClass("dark")
-  $(".box1").toggleClass("dark")
-  $("input").toggleClass("dark")
-  $(".box2").toggleClass("dark")
-  $(".download").toggleClass("dark")
-  $(".mode").toggleClass("dark")
-  $(".logo").toggleClass("dark")
-
-  if($(".mode").hasClass("dark")){
-    $(".mode").text("Light mode")
-  }else{
-    $(".mode").text("Dark mode")
-  }
-
-  if($(".logo").hasClass("dark")){
-    $(".logo img").attr("src","./images/logo-dark.png")
-  }else{
-    $(".logo img").attr("src","./images/logo-light.png")
-  }
-
-})
+  // 삼항 연산자
+  // 조건 ? 참일 때 실행 할 코드(true) : 거짓일 때(false)
+  darkToggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode';
+}
