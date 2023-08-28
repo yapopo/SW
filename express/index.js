473a533a3d4dad59981c4e76eaf02fc86fa90e8d
yapopo <dbrkdhk4120@naver.com>
2023-08-28 13:55:13 +0900

    🐛

--- express/index.js
+++ express/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.listen(7000, function(){
+  console.log('7000번 포트')
+})
+
+app.get('/',function (requests,response) {
+  response.sendFile(__dirname + '/index.html')
+})
