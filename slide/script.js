560a5ed43daf97d32abdced42ecbc2fa136cc5d2
yapopo <dbrkdhk4120@naver.com>
2023-08-31 12:16:53 +0900

    ⚡️ slider

---
+++ slide/script.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.slider-for').slick({
+      //메인슬라이드
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slider-nav',
+      autoplay: true,
+      autoplaySpeed : 2000,
+      infinite: false       
+  });
+
+  $('.slider-nav').slick({
+      //메인슬라이드 버튼
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      focusOnSelect: true,
+      autoplay: true,
+      infinite: false,
+      swipe: false,
+      swipeToSlide: false		
+  });
+});
