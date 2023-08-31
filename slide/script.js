2276a22ee925ecc2c9c36128398c39a8b43e19a8
yapopo <dbrkdhk4120@naver.com>
2023-09-01 08:57:10 +0900

    🔥 수정

--- slide/script.js
+++ slide/script.js
@@ -1,23 +1,22 @@
-$(function() {
-  $('.slider-for').slick({
-      //메인슬라이드
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      asNavFor: '.slider-nav',
-      autoplay: true,
-      autoplaySpeed : 2000,
-      infinite: false       
+$(".slider").slick({
+  dots: true,
+              arrows: false,
+              infinite: false,
+              slidesToShow: 2,
+              slidesToScroll: 2,
+  
+      //autoplay: true,
+      //dots: true
+      customPaging : function(slider, i) {
+          var title = $(slider.$slides[i]).data('title');
+          return '<a class="pager__item"> '+title+' </a>';
+      },
+  
+      //responsive: [{ 
+      //    breakpoint: 200,
+      //    settings: {
+      ///        d
+      //    } 
+      //}]
   });
-
-  $('.slider-nav').slick({
-      //메인슬라이드 버튼
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
-      focusOnSelect: true,
-      autoplay: true,
-      infinite: false,
-      swipe: false,
-      swipeToSlide: false		
-  });
-});
+  
