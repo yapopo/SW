96923e8eeed191d04c9d4131be5859b8d056092d
yapopo <dbrkdhk4120@naver.com>
2023-09-18 16:02:59 +0900

    ✨ 배열

--- React/01.JavascriptES6/02.js
+++ React/01.JavascriptES6/02.js
@@ -10,3 +10,9 @@ let arr = [1,2,3,4,5]
 
 let [num1,num2] = arr
 console.log(num1,num2)
+
+// 배열안 나머지 값 가져오기
+// ...이름은 자유롭게 지어도 되지만 주로 rest하는 이름으로 이용한다
+
+let [a, b, ...rest] = arr
+console.log(rest)
