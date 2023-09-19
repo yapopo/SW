2d86bbffc19ebfcc748d18dd280aa6aecd40434e
yapopo <dbrkdhk4120@naver.com>
2023-09-19 16:48:43 +0900

    ✨ useState bgColor

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -14,17 +14,39 @@ const State = () => {
   // let num = 0;
   let [count, setCount] = useState(0);
   let [title, setTitle] = useState('Jason');
+  let [name, setName] = useState(['이름1','이름2','이름3'])
+  let [bg, setBg] = useState('white');
+
+  let changeBg = ()=>{
+    let red = 'red';
+    setBg(red)
+  }
   
   return (
-    <div>
+    <div style={{background : bg}}>
       <h1>{count}</h1>
       <h1>{title}</h1>
       <button onClick={() => setCount(count+1)}>1씩증가</button>
       <button onClick={
+        // state변경 함수는 기존값 state가 가지고 있는 값을 비교해서 같은 값이면
+        // 변경하지 않고, 다른값이면 state함수 호출값으로 변경시켜준다
         ()=> {let titleCopy = [...title];
           titleCopy = 'Bruce';
           setTitle(titleCopy);
         }}>title바꾸기</button>
+
+        <h1>{name}</h1>
+        <button onClick={
+          ()=>{
+            let nameCopy = [...name]
+            nameCopy[0] = '이름4'
+            setName(nameCopy)
+          }
+        }>이름1변경</button>
+
+        <button onClick={changeBg}>배경색</button>
+
+
     </div>
   );
 }
