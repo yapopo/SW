7ed71d45af6e1c787060d11bfd31604b25730d4f
yapopo <dbrkdhk4120@naver.com>
2023-09-21 14:26:00 +0900

    🎉 header

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
-// import User from './component/User'
-// import Practice from './component/Practice';
-// import State from './component/State';
+
 
 function App() {
   // return()안에 html작성
   // 무족건 하나의 태그로 시작해서 끝내야햔다(병렬기입X)
   return (
     <div className="App">
-      {/* <User/>
-      <Practice/> */}
-      {/* <State></State> */}
+
     </div>
   );
 }
